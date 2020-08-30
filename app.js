89c9053ed953dda6a28dbf3f1e989df94578123e
CptSpooky <legacyofthedonut@gmail.com>
2020-08-29 20:52:14 -0400

    jest testing

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
 
-
-
-
 // array of questions for user
 const questions = [
 
@@ -39,20 +36,14 @@ const questions = [
     }
 ];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
 function employeeFactory(name, id, email, roleID) {
     switch (roleID) {
-        case 1: return new Manager(name, id, email);
-        case 2: return new Engineer(name, id, email);
-        case 3: return new Intern(name, id, email);
+        case 1: return new Manager(name, id, email, "");
+        case 2: return new Engineer(name, id, email, "");
+        case 3: return new Intern(name, id, email, "");
         default: return new Employee(name, id, email);
     }
 }
-
 
 // function to initialize program
 async function init() {
@@ -76,27 +67,13 @@ async function init() {
 
     }
 
-    
     const out = render(employees);
     writeToFile("./output/team.html", out);
-    console.log(out);
 
-    //system.log("check");
 }
 
 // function call to initialize program
 init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
 
 // function to write to html file
 function writeToFile(fileName, data) {
@@ -108,13 +85,3 @@ function writeToFile(fileName, data) {
         console.log("success!");
     });
 }
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
